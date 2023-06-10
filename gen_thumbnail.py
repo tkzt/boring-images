7e436f39c02ebfe52c0b83e423460303cab4f02b
陶清秋 <allen@taoqingqiudeMacBook-Air.local>
2023-06-10 14:36:56 +0800

    feat: improve thumbnail generating logic

--- gen_thumbnail.py
+++ gen_thumbnail.py
@@ -5,36 +5,38 @@ from pathlib import Path
 from itertools import chain
 
 REF_WIDTH = 300
-JSON_PATH = 'images.json'
+JSON_NAME = 'images.json'
+ARCHIVES = ['blog', 'chaotic', 'fine-weather']
 
-def run(folder='.'):
-  processed = {}
+def run():
+  for archive in ARCHIVES:
+    folder_path = Path(archive)
+    processed = {}
 
-  images_json_path = Path(JSON_PATH)
-  if images_json_path.exists():
-    with open(JSON_PATH, 'rt', encoding='utf-8') as f:
-      processed = json.load(f)
+    images_json_path = folder_path / JSON_NAME
+    if images_json_path.exists():
+      with open(images_json_path, 'rt', encoding='utf-8') as f:
+        processed = json.load(f)
+    
+    for img_path in list(chain.from_iterable([folder_path.glob(f'**/*.{i}') for i in ['png', 'jpg', 'jpeg']])):
+      path_str = str(img_path)
+      if path_str not in processed:
+        img = Image.open(img_path)
+        w, h = img.size
+        img.thumbnail((300, round(300/w*h)))
+
+        thumbnail_folder = folder_path / 'thumbnail'
+        thumbnail_folder.mkdir(exist_ok=True, parents=True)
+        img.save(thumbnail_folder / img_path.name)
+
+        hash = blurhash.encode(img, 4, 4)
+        processed[path_str] = {
+          'hash': hash,
+          'size': [w, h],
+        }
   
-  folder_path = Path(folder)
-  for img_path in list(chain.from_iterable([folder_path.glob(f'**/*.{i}') for i in ['png', 'jpg', 'jpeg']])):
-    path_str = str(img_path)
-    if path_str not in processed:
-      img = Image.open(img_path)
-      w, h = img.size
-      img.thumbnail((300, round(300/w*h)))
-
-      thumbnail_folder = img_path.parent / 'thumbnail'
-      thumbnail_folder.mkdir(exist_ok=True, parents=True)
-      img.save(thumbnail_folder / img_path.name)
-
-      hash = blurhash.encode(img, 4, 4)
-      processed[path_str] = {
-        'hash': hash,
-        'size': [w, h]
-      }
-  
-  with open(JSON_PATH, 'wt', encoding='utf-8') as f:
-    f.write(json.dumps(processed))
+    with open(images_json_path, 'wt', encoding='utf-8') as f:
+      f.write(json.dumps(processed))
     
 if __name__ == '__main__':
   run()
